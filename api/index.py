6859e1b23074c0a4396c061dee52be81425e1b41
Ethan Hasbrouck <ethanhasbrouck02@gmail.com>
2023-02-03 06:02:51 -0800

    Add jsonify

--- api/index.py
+++ api/index.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
-
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        question = request.form.get('question')
+        history = request.form.get('history')
+        response = {'question': question, 'history': history}
+        return jsonify(response)
+    else:
+        # handle GET request
+        response = {'message': 'GET request received'}
+        return jsonify(response)
