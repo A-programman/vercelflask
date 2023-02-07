d484e0ff4a4f8f01f9d9a9da4b48c9739aeeac15
Ethan Hasbrouck <ethanhasbrouck02@gmail.com>
2023-02-07 09:49:51 -0800

    Switch

--- api/index.py
+++ api/index.py
@@ -1,15 +1,31 @@
+
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+from langchain.llms import OpenAI
+
 from flask import Flask, request, jsonify
 
+import os
+os.environ["OPENAI_API_KEY"] = "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"
+os.environ["SERPAPI_API_KEY"] = "684035001579d1e54d72e18206f5e7d46c1a7d30ddf0ee213e7314692b668a5f"
 app = Flask(__name__)
 
+def generate_response(assertion):
+    llm = OpenAI(temperature=0)
+    tools = load_tools(["serpapi"], llm=llm)
+    agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
+
+    response = agent.run("Find and quote an excerpt from an article from PubMed to support this assertion, once found state the authors, titles and source (Ask yourself if the found article excerpt actually supports the assertion, if you can't find it after a few searches, then there is none found): " + assertion)
+
+    return response
+
 @app.route('/', methods=['GET', 'POST'])
-def index():
+def handle_request():
     if request.method == 'POST':
-        question = request.form.get('question')
-        history = request.form.get('history')
-        response = {'question': question, 'history': history}
-        return jsonify(response)
-    else:
-        # handle GET request
-        response = {'message': 'GET request received'}
-        return jsonify(response)
+        assertion = request.json.get('assertion')
+        response = generate_response(assertion)
+        return jsonify({'response': response})
+    elif request.method == 'GET':
+        assertion = request.args.get('assertion')
+        response = generate_response(assertion)
+        return jsonify({'response': response})
