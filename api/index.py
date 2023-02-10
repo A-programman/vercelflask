e691bd8d3756a13fb01ad9822e529c21a0e2fa88
Ethan Hasbrouck <ethanhasbrouck02@gmail.com>
2023-02-10 11:45:23 -0800

    Change to 1 result

--- api/index.py
+++ api/index.py
@@ -18,7 +18,7 @@ def generate_reference(prompt):
     return agent.run("Find an excerpt from an article from PubMed to support this assertion, (Ask yourself if the found article excerpt actually supports the assertion, if you can't find it after a few searches, then reply with NONE FOUND): " + prompt)
 
 def generate_response(prompt):
-    return search.results(prompt, 5)
+    return search.results(prompt, 1)
 
 @app.route('/', methods=['GET', 'POST'])
 def handle_request():
