6705c410c94724871946dd59a9645c1268f3fe33
Ethan Hasbrouck <ethanhasbrouck02@gmail.com>
2023-02-10 10:41:06 -0800

    Prompt update

--- api/index.py
+++ api/index.py
@@ -15,7 +15,7 @@ search = GoogleSearchAPIWrapper()
 app = Flask(__name__)
 
 def generate_reference(prompt):
-    return agent.run("Find an excerpt from an article from PubMed to support this assertion, (Ask yourself if the found article excerpt actually supports the assertion, if you can't find it after a few searches, then there is none found): " + prompt)
+    return agent.run("Find an excerpt from an article from PubMed to support this assertion, (Ask yourself if the found article excerpt actually supports the assertion, if you can't find it after a few searches, then reply with NONE FOUND): " + prompt)
 
 def generate_response(prompt):
     return search.results(prompt, 5)
