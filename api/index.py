3c7149ae158b394cccc93403ad1165b7f8eb18cf
Ethan Hasbrouck <ethanhasbrouck02@gmail.com>
2023-02-10 09:58:12 -0800

    S

--- api/index.py
+++ api/index.py
@@ -6,11 +6,6 @@ from langchain.agents import initialize_agent, Tool, load_tools
 from langchain.llms import OpenAI
 from langchain.utilities import GoogleSearchAPIWrapper
 
-os.environ["OPENAI_API_KEY"] = "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"
-os.environ["SERPAPI_API_KEY"] = "f15b49da3c7d3b44f53e7c3464742627083a628070e6242d7206c372bde15ec1"
-os.environ["GOOGLE_API_KEY"] = "AIzaSyACujhbTZfoeNbvNmkZ3nVNLHAs8kQXmhA"
-os.environ["GOOGLE_CSE_ID"] = "56a78a900f119419b"
-
 llm = OpenAI(temperature=0)
 tools = load_tools(["serpapi"], llm=llm)
 agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
