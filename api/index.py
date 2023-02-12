93e1ba2c56251ac2fcbe0ba23dcb7a75a4f22104
Ethan Hasbrouck <ethanhasbrouck02@gmail.com>
2023-02-12 00:02:55 -0800

    Rerun

--- api/index.py
+++ api/index.py
@@ -15,10 +15,13 @@ search = GoogleSearchAPIWrapper()
 app = Flask(__name__)
 
 def generate_reference(prompt):
-    return agent.run("Find an excerpt from an article from PubMed to support this assertion, (Ask yourself if the found article excerpt actually supports the assertion, if you can't find it after a few searches, then reply with a suggested revision, with square brackets around the revision): " + prompt)
+    return agent.run("Find an excerpt from an article from PubMed to support this assertion, (Ask yourself if the found article excerpt actually supports the assertion, if you can't find it after a few searches, then reply with NONE FOUND): " + prompt)
 
 def generate_response(prompt):
     return search.results(prompt, 1)
+
+def generate_revision(prompt):
+    return agent.run("Revise this statement to be accurate to medical information from PubMed:" + prompt)
 
 @app.route('/', methods=['GET', 'POST'])
 def handle_request():
@@ -27,7 +30,8 @@ def handle_request():
         reference = generate_reference(assertion)
         print("REFERENCE: " + reference)
         if reference == "NONE FOUND":
-            return jsonify("NONE FOUND")
+            revision = generate_revision(assertion)
+            return jsonify([{"title": "Revision", "revision": revision}])
         else:
             response = generate_response(reference)
             return jsonify(response)
