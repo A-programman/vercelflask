8c19bd2fa406aa7fd8e7cda9bbf575d2ae9ba5bc
Ethan Hasbrouck <ethanhasbrouck02@gmail.com>
2023-02-14 21:49:13 -0800

    Change result number and select scholar or pubmed

--- api/index.py
+++ api/index.py
@@ -15,10 +15,10 @@ search = GoogleSearchAPIWrapper()
 app = Flask(__name__)
 
 def generate_reference(prompt):
-    return agent.run("Find an excerpt from an article from PubMed to support this assertion, (Ask yourself if the found article excerpt actually supports the assertion, if you can't find it after a few searches, then reply with NONE FOUND): " + prompt)
+    return agent.run("Find an excerpt from an article from ONLY PubMed or Google Scholar to support this assertion, (Ask yourself if the found article excerpt actually supports the assertion, if you can't find it after a few searches, then reply with NONE FOUND): " + prompt)
 
 def generate_response(prompt):
-    return search.results(prompt, 1)
+    return search.results(prompt + "Pubmed, Google Scholar", 3)
 
 def generate_revision(prompt):
     return agent.run("Revise this statement to be accurate to medical information from PubMed:" + prompt)
